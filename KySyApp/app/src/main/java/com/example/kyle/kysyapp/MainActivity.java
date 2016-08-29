8ef9f15ca08042f607e1fd80f8805ce7f1386945
Hagoodkk <Hagood.Kyle@gmail.com>
2016-08-29 14:52:08 -0400

    Added Button

--- KySyApp/app/src/main/java/com/example/kyle/kysyapp/MainActivity.java
+++ KySyApp/app/src/main/java/com/example/kyle/kysyapp/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.kyle.kysyapp;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button syedButton = (Button) findViewById(R.id.Syed_Button);
+        syedButton.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "Syed pities da fool", Toast.LENGTH_LONG).show();
     }
 }
